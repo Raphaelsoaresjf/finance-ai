918e2959eb3a94ff68ff9fb1ecea2dc7fa36d64d
Raphael Soares <raphael.moreira@meuinc.com.br>
2024-11-08 09:52:47 -0300

    feat: ensure transactions ans subscriptions pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,11 +1,23 @@
+import { auth } from "@clerk/nextjs/server";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionColumns } from "./_columns";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transaction = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const transaction = await db.transaction.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+
   return (
     <>
       <Navbar />
